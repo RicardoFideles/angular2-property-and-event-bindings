0e90d257b6e15ef9b7cc2b3276a54803e167febb
Ricardo Jorge Fideles Junior <ricardo@ricardofideles.com.br>
2017-03-26 16:09:09 -0300

    working with both event and properties binding

---
+++ .history/src/app/data-binding/data-binding.component_20170326155241.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url:string = 'http://loaiane.com';
+
+  cursoAngular:boolean = true;
+
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  onClick() {
+    alert('botao clicado!')
+  }
+
+  onKeyup(event:KeyboardEvent) {
+    console.log(event.);
+  }
+
+}
---
+++ .history/src/app/data-binding/data-binding.component_20170326160504.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url:string = 'http://loaiane.com';
+
+  cursoAngular:boolean = true;
+
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  conteudoAtual:string = '';
+  conteudoSalvo:string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  onClick() {
+    alert('botao clicado!')
+  }
+
+  onKeyup(event:KeyboardEvent) {
+    console.log(event);
+    this.conteudoAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  onSave(valor:string) {
+    this.conteudoSalvo = valor;
+  }
+
+  onMouseSpan() {
+    this.isMouseOver = !this.is
+  }
+
+}
